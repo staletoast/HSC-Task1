553a8e9d2fbfed802be58004485a88456c65466b
Melody T <124547562+staletoast@users.noreply.github.com>
2025-02-01 11:01:34 +0000

    m

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def countdown():
         time.sleep(1)
         timeout_duration -= 1
     print("Timeout reached!")
-    sys.exit
+    exit()
 
 @app.route("/success.html", methods=["POST", "GET", "PUT", "PATCH", "DELETE"])
 def addFeedback():
